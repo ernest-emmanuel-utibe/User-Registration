86d4374a383442dfe08881193e388f4355cce3d8
Ernest Emmanuel Utibe <em123nest@gmail.com>
2024-01-25 07:43:58 +0100

    Update UserService.java

--- src/main/java/com/secure/userRegAndAuthSys/service/UserService.java
+++ src/main/java/com/secure/userRegAndAuthSys/service/UserService.java
@@ -2,10 +2,8 @@ package com.secure.userRegAndAuthSys.service;
 
 import com.secure.userRegAndAuthSys.controller.registration.RegistrationRequest;
 import com.secure.userRegAndAuthSys.data.models.User;
-
 import java.util.List;
 import java.util.Optional;
-
 
 /**
  * @author Ernest Emmanuel Utibe
